61b2cb81f192792ef1ac71ecbf7b325fc0baa6bf
Michael <micsamkid500@gmail.com>
2026-09-15 12:04:39 -0500

    adding provision feature to Jenkinsfile

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -1,9 +1,4 @@
 
-data "aws_eks_cluster" "cluster" { //retrieves auth token
-    name = module.eks.cluster_name
-
-}
-
 data "aws_eks_cluster_auth" "cluster" { //retrieves auth token
     name = module.eks.cluster_name
 
@@ -12,7 +7,7 @@ data "aws_eks_cluster_auth" "cluster" { //retrieves auth token
 
 
 provider "kubernetes" {
-    host = data.aws_eks_cluster.cluster.endpoint
+    host = module.eks.cluster_endpoint
     token = data.aws_eks_cluster_auth.cluster.token
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
@@ -42,7 +37,7 @@ resource "kubernetes_storage_class_v1" "gp3" {
 provider "helm" {
 
     provider "kubernetes" {
-    host = data.aws_eks_cluster.cluster.endpoint
+    host = module.eks.cluster_endpoint
     token = data.aws_eks_cluster_auth.cluster.token
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
