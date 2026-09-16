4dea95967901cbbd7ae0930dfb1e45b71f84e939
Michael <micsamkid500@gmail.com>
2026-09-16 18:49:42 -0500

    adding secrets and cleaning up mysql deployment

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -76,24 +76,16 @@ provider "helm" {
 }
 
 
-
-# user_data = <<EOF
-    
-#     #!/bin/bash
-#     kubectl create secret generic mysql-creds --from-literal=mysql-root-password=${var.mysql_root_password} --from-literal=mysql-password=${var.mysql_user_password} --from-literal=mysql-replication-password=${var.mysql_replication_password}
-
-#     EOF
-
 resource "helm_release" "mysql" {
     name = "mysql-release"
     repository = "https://charts.bitnami.com/bitnami"
     chart = "mysql"
-    version = "14.0.3" //watch this version
+    version = "14.0.3" 
     timeout = 120
 
     // add values file here
 
-    values = ["${file("helmvalues-mysql.yaml")}"]
+    values = ["${file("helmvalues-myssssql.yaml")}"]
     
 
     set {
