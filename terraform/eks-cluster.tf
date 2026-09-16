9eb9b9b23dd1aba5e3d5c6179a77015ceee27917
Michael <micsamkid500@gmail.com>
2026-09-16 13:03:04 -0500

    adding provision feature to Jenkinsfile

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -15,6 +15,22 @@ module "eks" {
 
   }
 
+  module "aws_ebs_csi_pod_identity" {
+    source = "terraform-aws-modules/eks-pod-identity/aws"
+
+    name = "aws-ebs-csi"
+
+    attach_aws_ebs_csi_policy = true
+
+    associations = {
+      this = {
+        cluster_name = module.eks.cluster_name
+        namespace = "kube-system"
+        service_account = "ebs-csi-controller-sa"
+      }
+    }
+  }
+
   name = "company-cluster"
   kubernetes_version = 1.36
 
