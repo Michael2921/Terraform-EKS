db1d6f0347f66d2bf0e3465482551b01e3fd0d2d
Michael <micsamkid500@gmail.com>
2026-09-17 16:00:29 -0500

    adding company app to deployments

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -13,7 +13,7 @@ module "company-vpc" {
     cidr = "10.0.0.0/16" 
     private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]  
     public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"] 
-    azs = data.aws_availability_zones.azs.names
+    azs = slice(data.aws_availability_zones.azs.names, 0, 3)
 
     map_public_ip_on_launch = true
 
