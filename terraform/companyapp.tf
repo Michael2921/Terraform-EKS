2e2cb7685944867200956216f5aaa963424414fa
Michael <micsamkid500@gmail.com>
2026-09-17 17:13:50 -0500

    adding company app to deployments

--- terraform/companyapp.tf
+++ terraform/companyapp.tf
@@ -1,56 +1,56 @@
-# variable "docker_username" {
-#   type = string
-# }
+variable "docker_username" {
+  type = string
+}
 
-# variable "docker_PAT" {
-#   type = string
-# }
+variable "docker_PAT" {
+  type = string
+}
 
 
-# resource "kubernetes_secret_v1" "docker-creds" {
-#     depends_on = [module.eks]
-#     metadata {
-#         name = "docker-creds"
-#         namespace = "fargate"
-#     }
+resource "kubernetes_secret_v1" "docker-creds" {
+    depends_on = [module.eks]
+    metadata {
+        name = "docker-creds"
+        namespace = "fargate"
+    }
 
-#     type = "kubernetes.io/dockerconfigjson"
+    type = "kubernetes.io/dockerconfigjson"
 
-#     data = {
-#         ".dockerconfigjson" = jsonencode({
-#             auths = {
-#                 "https://index.docker.io/v1/" = {
-#                     username = var.docker_username
-#                     password = var.docker_PAT
-#                     auth = base64encode("${var.docker_username}:${var.docker_PAT}")
-#                 }
-#             }
-#         })
-#     }
+    data = {
+        ".dockerconfigjson" = jsonencode({
+            auths = {
+                "https://index.docker.io/v1/" = {
+                    username = var.docker_username
+                    password = var.docker_PAT
+                    auth = base64encode("${var.docker_username}:${var.docker_PAT}")
+                }
+            }
+        })
+    }
 
 
     
-# }
+}
 
 
-# resource "kubernetes_manifest" "companyapp_configmap" {
-#   depends_on = [module.eks]
-#   manifest = yamldecode(
-#     file("companyapp-configmap.yaml")
-#   )
-# }
+resource "kubernetes_manifest" "companyapp_configmap" {
+  depends_on = [module.eks]
+  manifest = yamldecode(
+    file("companyapp-configmap.yaml")
+  )
+}
 
 
-# resource "kubernetes_manifest" "companyapp_deployment" {
-#   depends_on = [module.eks]
-#   manifest = yamldecode(
-#     file("companyapp-deployment.yaml")
-#   )
-# }
+resource "kubernetes_manifest" "companyapp_deployment" {
+  depends_on = [module.eks]
+  manifest = yamldecode(
+    file("companyapp-deployment.yaml")
+  )
+}
 
-# resource "kubernetes_manifest" "companyapp_service" {
-#   depends_on = [module.eks]
-#   manifest = yamldecode(
-#     file("companyapp-service.yaml")
-#   )
-# }
+resource "kubernetes_manifest" "companyapp_service" {
+  depends_on = [module.eks]
+  manifest = yamldecode(
+    file("companyapp-service.yaml")
+  )
+}
