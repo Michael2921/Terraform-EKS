e349d7cb6194defade249360e50989325d03baa1
Michael <micsamkid500@gmail.com>
2026-09-16 20:10:58 -0500

    adding secrets and cleaning up mysql deployment

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -66,9 +66,8 @@ resource "kubernetes_secret_v1" "mysql_creds" {
 
 
 
-
 provider "helm" {
-     kubernetes {
+     kubernetes = {
     host = module.eks.cluster_endpoint
     token = data.aws_eks_cluster_auth.cluster.token
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
