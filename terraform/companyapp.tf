f1baccb22b3f028d6b13ece8b45f4b0d9fad8d39
Michael <micsamkid500@gmail.com>
2026-09-17 14:09:19 -0500

    adding company app to deployments

--- terraform/companyapp.tf
+++ terraform/companyapp.tf
@@ -48,6 +48,6 @@ resource "kubernetes_manifest" "companyapp_deployment" {
 
 resource "kubernetes_manifest" "companyapp_service" {
   manifest = yamldecode(
-    file("companyapp-servi.yaml")
+    file("companyapp-service.yaml")
   )
 }
