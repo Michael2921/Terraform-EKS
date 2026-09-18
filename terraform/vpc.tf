c2f097aa371743db2376bbdc5bf2ebf10db90770
Michael <micsamkid500@gmail.com>
2026-09-17 23:35:32 -0500

    resolving loadbalancer issues

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -22,18 +22,18 @@ module "company-vpc" {
     enable_dns_hostnames = true
 
     tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/cluster/company-cluster" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/cluster/company-cluster" = "shared"
     "kubernetes.io/role/elb" = "1"
 
 
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/myapp-eks-cluster" = "shared"
+    "kubernetes.io/cluster/company-cluster" = "shared"
     "kubernetes.io/role/internal-elb" = "1"
 
 }
